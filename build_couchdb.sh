9add5484285d2b782cbe6330cf1aa25df7a7d480
David Halls <dahalls@gmail.com>
2014-06-10 22:01:52 +0100

    Initial commit

---
+++ build_couchdb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+cd "$HOME"
+git clone git://github.com/jhs/build-couchdb.git
+cd build-couchdb
+git submodule init
+git submodule update
+echo '@setfilename autoconf.info' > dependencies/autoconf-2.69/doc/autoconf.texi
+echo '@setfilename automake.info' > dependencies/automake-1.11.2/doc/automake.texi
+( cd dependencies/autoconf-archive; ed cfg.mk <<< $'g/@diff/d\nw'; git commit -a -m 'fix build' )
+git commit -a -m 'fix build'
+rake
