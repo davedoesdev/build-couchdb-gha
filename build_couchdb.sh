7a89137db5d70d87c7a15176b159c3a70d158524
David Halls <dahalls@gmail.com>
2015-10-06 22:20:31 +0100

    Use @zpetr fork add update to Couch 1.6.1 and OTP 8.1

--- build_couchdb.sh
+++ build_couchdb.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
-git clone git://github.com/jhs/build-couchdb.git
+git clone git://github.com/zpetr/build-couchdb.git
 cd build-couchdb
+git checkout otp-fix
 git submodule init
 git submodule update
+( cd dependencies/couchdb; git checkout 1.6.1 )
+( cd dependencies/otp; git checkout OTP-8.1 )
 echo '@setfilename autoconf.info' > dependencies/autoconf-2.69/doc/autoconf.texi
 echo '@setfilename automake.info' > dependencies/automake-1.11.2/doc/automake.texi
 ( cd dependencies/autoconf-archive; ed cfg.mk <<< $'g/@diff/d\nw'; git commit -a -m 'fix build' )
