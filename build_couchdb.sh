138755dfd43e92b8fd20589d68fa4e4f697f8977
David Halls <dahalls@gmail.com>
2016-04-27 23:45:01 +0100

    Rev OTP version

--- build_couchdb.sh
+++ build_couchdb.sh
@@ -6,7 +6,7 @@ git checkout otp-fix
 git submodule init
 git submodule update
 ( cd dependencies/couchdb; git checkout 1.6.1 )
-( cd dependencies/otp; git checkout OTP-17.5.6.4 )
+( cd dependencies/otp; git checkout OTP-18.3.2 )
 echo '@setfilename autoconf.info' > dependencies/autoconf-2.69/doc/autoconf.texi
 echo '@setfilename automake.info' > dependencies/automake-1.11.2/doc/automake.texi
 ( cd dependencies/autoconf-archive; ed cfg.mk <<< $'g/@diff/d\nw'; git commit -a -m 'fix build' )
