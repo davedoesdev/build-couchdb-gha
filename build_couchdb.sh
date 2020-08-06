f4dad385752348dc4445849a43649d588172152b
David Halls <dahalls@gmail.com>
2020-08-06 17:55:21 +0100

    CouchDB 3.1.0

--- build_couchdb.sh
+++ build_couchdb.sh
@@ -3,7 +3,7 @@ curdir="$(cd "$(dirname "$0")"; echo $PWD)"
 set -e
 git clone https://github.com/apache/couchdb
 cd couchdb
-#git checkout 2.3.1
+git checkout 3.1.0
 wget -O - http://ftp.mozilla.org/pub/mozilla.org/js/js185-1.0.0.tar.gz | tar zx
 patch -p0 < "$curdir/js-1.8.5.patch"
 pushd js-1.8.5/js/src
