9338cd54181f880b1c02a8ca7c1c01606991f8af
David Halls <dahalls@gmail.com>
2022-06-02 15:24:03 +0100

    Fix build on Ubuntu 22.04

--- build_couchdb.sh
+++ build_couchdb.sh
@@ -3,7 +3,7 @@ curdir="$(cd "$(dirname "$0")"; echo $PWD)"
 set -e
 git clone https://github.com/apache/couchdb
 cd couchdb
-git checkout 3.2.0
+git checkout 3.2.2
 wget -O - http://ftp.mozilla.org/pub/mozilla.org/js/js185-1.0.0.tar.gz | tar zx
 patch -p0 < "$curdir/js-1.8.5.patch"
 pushd js-1.8.5/js/src
@@ -11,5 +11,6 @@ pushd js-1.8.5/js/src
 make
 popd
 ./configure
-make release ERL_CFLAGS="-I$PWD/js-1.8.5/js/src -I/usr/lib/erlang/usr/include" LIBRARY_PATH="$PWD/js-1.8.5/js/src/dist/lib"
+patch -p1 -d src/docs < "$curdir/couchdb-documentation.patch"
+NODE_OPTIONS=--openssl-legacy-provider make release ERL_CFLAGS="-I$PWD/js-1.8.5/js/src -I/usr/lib/erlang/usr/include" LIBRARY_PATH="$PWD/js-1.8.5/js/src/dist/lib"
 cp js-1.8.5/js/src/dist/lib/libmozjs185.so rel/couchdb/lib/libmozjs185.so.1.0
